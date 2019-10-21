fe2737b885a58aba457861a71b76e0b0e5cddace
Aleksander Harhun <aleksanderharhun@gmail.com>
2019-10-21 12:20:17 +0200

    Updates

---
+++ precache-manifest.4f1b4fc5ae9af4cf953ef87f9cec0334.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36424ad5d3464a228cd5eeea8a9acc9d",
+    "url": "/Loader-generator/index.html"
+  },
+  {
+    "revision": "3c363b942fd7cc9aa15e",
+    "url": "/Loader-generator/static/css/main.0f371cba.chunk.css"
+  },
+  {
+    "revision": "276f76a28f97c0178d91",
+    "url": "/Loader-generator/static/js/2.9a5c790f.chunk.js"
+  },
+  {
+    "revision": "3c363b942fd7cc9aa15e",
+    "url": "/Loader-generator/static/js/main.c7fbf0ad.chunk.js"
+  },
+  {
+    "revision": "c3c58389cf6ae1b157f6",
+    "url": "/Loader-generator/static/js/runtime-main.d50a12dc.js"
+  }
+]);
